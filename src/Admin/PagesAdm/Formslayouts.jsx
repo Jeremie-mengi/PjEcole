371b9a182e37ce1ae3927fffb121a1a518559542
Jeremie-mengi <jeremiemengi248@gmail.com>
2025-01-20 09:38:58 +0100

    myproject

--- src/Admin/PagesAdm/Formslayouts.jsx
+++ src/Admin/PagesAdm/Formslayouts.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 function Formslayouts() {
   return (
@@ -229,7 +231,126 @@ function Formslayouts() {
   </ul>
 </nav>
 
-</header>   
+</header>  
+
+<aside id="sidebar" className="sidebar">
+
+<ul className="sidebar-nav" id="sidebar-nav">
+
+  <li className="nav-item">
+    <a className="nav-link " href="index.html">
+      <i className="bi bi-grid"></i>
+      <span>Tableau de bord </span>
+    </a>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+      <i className="bi bi-journal-text"></i><span>Inscription de L'eleve</span><i className="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="forms-nav" className="nav-content collapse " >
+    <li>
+    <Link to="/formlay">
+     <i className="bi bi-circle"></i><span>Formulaire d'inscription</span>
+   </Link>
+  </li>
+
+
+    </ul>
+  </li>
+  
+  <li className="nav-item">
+    <a className="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+      <i className="bi bi-layout-text-window-reverse"></i><span>Etats des eleves</span><i className="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="tables-nav" className="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="tables-general.html">
+          <i className="bi bi-circle"></i><span> Liste des élèves.</span>
+        </a>
+      </li>
+      <li>
+        <a href="tables-data.html">
+          <i className="bi bi-circle"></i><span>Presences</span>
+        </a>
+      </li>
+      <li>
+        <a href="tables-data.html">
+          <i className="bi bi-circle"></i><span>Absences</span>
+        </a>
+      </li>
+    </ul>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+      <i className="bi bi-menu-button-wide"></i><span>Components</span><i className="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" className="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="components-carousel.html">
+          <i className="bi bi-circle"></i><span>Carousel</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-list-group.html">
+          <i className="bi bi-circle"></i><span>List group</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-modal.html">
+          <i className="bi bi-circle"></i><span>Modal</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tabs.html">
+          <i className="bi bi-circle"></i><span>Tabs</span>
+        </a>
+      </li>
+    </ul>
+  </li>
+
+  <li className="nav-heading">Pages</li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" href="users-profile.html">
+      <i className="bi bi-person"></i>
+      <span>Profile</span>
+    </a>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" href="pages-faq.html">
+      <i className="bi bi-question-circle"></i>
+      <span>F.A.Q</span>
+    </a>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" href="pages-contact.html">
+      <i className="bi bi-envelope"></i>
+      <span>Contact</span>
+    </a>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" href="pages-register.html">
+      <i className="bi bi-card-list"></i>
+      <span>Register</span>
+    </a>
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link collapsed" href="pages-login.html">
+      <i className="bi bi-box-arrow-in-right"></i>
+      <span>Login</span>
+    </a>
+  </li>
+
+</ul>
+
+</aside>
+
       
     </>
   )
