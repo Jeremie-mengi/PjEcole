da692f77938004e20760677b05da6fc22c39174a
Jeremie-mengi <jeremiemengi248@gmail.com>
2025-03-04 16:04:12 +0100

    create-Ele

--- routes/countPresences.js
+++ routes/countPresences.js
@@ -2,4 +2,4 @@ const express = require("express")
 const {getPresence} = require ("../controllers/countPresences")
 const router = express.Router();
 router.get("/presences",getPresence )
-module.exports= router
+module.exports= routerr
